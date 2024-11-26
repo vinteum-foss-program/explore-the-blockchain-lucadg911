6324bb1fd6b3c38154e5e1dc441bf0d35bfb07f5
Luca DG <lucagontijo911@outlook.com>
2024-11-26 14:40:08 -0300

    solved 001

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,9 @@
-# What is the hash of block 654,321?
+#!/bin/bash
+USER="-rpcuser=user_106"
+PASSWORD="-rpcpassword=4wjt7NoqJHld"
+IP="-rpcconnect=84.247.182.145"
+
+HEIGHT=654321
+
+BLOCK_HASH=$(bitcoin-cli $USER $PASSWORD $IP getblockhash "$HEIGHT")
+echo "$BLOCK_HASH"
