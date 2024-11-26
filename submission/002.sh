bad09734703091e939ffaa13eae3827d5b38fcdc
Luca DG <lucagontijo911@outlook.com>
2024-11-26 14:42:00 -0300

    solved 002

--- submission/002.sh
+++ submission/002.sh
@@ -1,4 +1,11 @@
-# (true / false) Verify the signature by this address over this message:
-#   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
-#   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
-#   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
+#!/bin/bash
+USER="-rpcuser=user_106"
+PASSWORD="-rpcpassword=4wjt7NoqJHld"
+IP="-rpcconnect=84.247.182.145"
+
+ADDRESS="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+MESSAGE="1E9YwDtYf9R29ekNAfbV7MvB4LN"
+SIGNATURE="HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+
+MESSAGE_VERIFY=$(bitcoin-cli $USER $PASSWORD $IP verifymessage "$ADDRESS" "$SIGNATURE" "$MESSAGE")
+echo "$MESSAGE_VERIFY"
